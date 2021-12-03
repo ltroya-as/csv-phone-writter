a0e5ffec421074b47e22b0ad58aeab7a093307d7
ltroya-as <luis.troya@allansolutions.com>
2021-12-02 21:05:44 -0500

    feat: add example

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = createCsvWriter({
     path: './file.csv',
     header: [
-        { id: 'phone', title: 'PHONE' },
+        { id: 'Location', title: 'location' },
+        { id: 'Year', title: 'year' },
+        { id: 'Production', title: 'production' },
     ]
 });
 
 const records = [
-    { phone: '+3132511492' },
-    { phone: '+5825487963' }
+    {
+        "Location": "LCO",
+        "Year": 110510,
+        "Production": "+20.63%"
+    },
+    {
+        "Location": "LFP",
+        "Year": 295450,
+        "Production": "+94.12%"
+    }
 ];
 
 csvWriter.writeRecords(records)       // returns a promise
